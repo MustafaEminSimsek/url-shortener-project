09498023e49fb67b6bd720774fcf77770c48f52b
mırcın <40897553+mircin@users.noreply.github.com>
2025-05-22 04:07:57 +0300

    asgfdsag

---
+++ Entities/redirectLog.cs
@@ -0,0 +1,13 @@
+﻿using System.Xml.Serialization;
+
+namespace UrlShortenerProject.Entities
+{
+    public class redirectLog
+    {
+        public int Id { get; set; }
+        public DateTime AccessedAt { get; set; } = DateTime.UtcNow;
+
+        public int urlShorteningId { get; set; }
+        public urlShortening urlShortening { get; set; }
+    }
+}
---
+++ Entities/urlShortening.cs
@@ -0,0 +1,11 @@
+﻿namespace UrlShortenerProject.Entities
+{
+    public class urlShortening
+    {
+        public int Id { get; set; }
+        public string OriginalUrl { get; set; }
+        public string ShortUrl { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public List<redirectLog> AccessLogs { get; set; }
+    }
+}
